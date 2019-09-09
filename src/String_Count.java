decd30e989137c849302c772c96923a9bffb5f4b
nndieu <nndieu88@gmail.com>
2019-09-09 22:26:23 +0700

    SS2

--- src/String_Count.java
+++ src/String_Count.java
@@ -2,20 +2,16 @@ import java.util.Scanner;
 
 public class String_Count {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String name, check;
-        int count = 0;
-
-        System.out.println("Enter string name: ");
-        name = scanner.nextLine();
-        System.out.println("Enter to check string name: ");
-        check = scanner.nextLine();
-
-        for (int i = 0; i < name.length(); i++) {
-            if (check.equals(name)) {
+        Scanner scanner=new Scanner(System.in);
+        char a='n';
+        String name="nguyen ngoc dieu";
+        int count=0;
+        for (int i=0;i<name.length();i++){
+            char result=name.charAt(i);
+            if (a==result){
                 count++;
             }
         }
-        System.out.println(count);
+        System.out.println("Result: " + count);
     }
 }
